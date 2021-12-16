46786962a54292c21074c117214674ede418c93f
Yongveloper <aaa9015@naver.com>
2021-12-16 22:50:12 +0900

    Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -35,26 +35,50 @@ import { useForm } from 'react-hook-form';
 // }
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
   return (
     <div>
-      <form>
-        <input {...register('email')} type="email" placeholder="Email" />
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={handleSubmit(onValid)}
+      >
         <input
-          {...register('firstName')}
+          {...register('email', { required: true })}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          {...register('firstName', { required: true })}
           type="text"
           placeholder="First Name"
         />
-        <input {...register('last_Name')} type="text" placeholder="Last Name" />
-        <input {...register('userName')} type="text" placeholder="Username" />
         <input
-          {...register('password')}
+          {...register('last_Name', { required: true })}
+          type="text"
+          placeholder="Last Name"
+        />
+        <input
+          {...register('userName', { required: true, minLength: 10 })}
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          {...register('password', { required: true, minLength: 5 })}
           type="password"
           placeholder="Password"
         />
         <input
-          {...register('password1')}
+          {...register('password1', {
+            required: 'Password is required',
+            minLength: {
+              value: 5,
+              message: 'Your password is too short',
+            },
+          })}
           type="password"
           placeholder="Password1"
         />
