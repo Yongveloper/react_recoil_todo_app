fc99377425ac953e315c804a1deb71b2c976bb2c
Yongveloper <aaa9015@naver.com>
2021-12-21 22:49:06 +0900

    Enums

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { categoryState, toDoSelector } from '../atom';
+import { Categories, categoryState, toDoSelector } from '../atom';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
@@ -10,7 +10,7 @@ function ToDoList() {
   const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
     const {
       currentTarget: { value },
-    } = event;
+    } = event as any;
     setCategory(value);
   };
   return (
@@ -18,9 +18,9 @@ function ToDoList() {
       <h1>To Dos</h1>
       <hr />
       <select value={category} onInput={onInput}>
-        <option value="TO_DO">To Do</option>
-        <option value="DOING">Doing</option>
-        <option value="DONE">Done</option>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
       </select>
       <CreateToDo />
       {toDos?.map((toDo) => (
